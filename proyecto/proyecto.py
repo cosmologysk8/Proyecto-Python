c7731d83f3f6e107ab306761ebd16d779853df92
AdrianFG <a1999afg@gmail.com>
2021-11-20 18:19:35 +0100

    he he creado una función para restar

--- proyecto/proyecto.py
+++ proyecto/proyecto.py
@@ -1,10 +1,9 @@
-from tkinter import font
-from bs4 import BeautifulSoup
-import bs4
-from bs4.builder import HTMLTreeBuilder
 from tkinter import *
-from tkinter import ttk
+from tkinter import font, ttk
+import bs4
 import requests
+from bs4 import BeautifulSoup
+from bs4.builder import HTMLTreeBuilder
 from requests.models import parse_url
 
 window = Tk()
@@ -18,8 +17,10 @@ url_pagina = 'https://www.theroomsk8.com/categoria-producto/skateshop/tablas/'
 tablas_skate = {
     "url_imagen":"",
     "nombre": "",
+    "precio": "",
     "boton_acceder": "",
 }
+
 
 def cargar_pagina():
     
@@ -32,7 +33,9 @@ def cargar_elementos():
     pagina = cargar_pagina()
     list_elementos = pagina.findAll("div",{"class": "product type-product post-29781 status-publish first instock product_cat-skateshop product_cat-tablas product_tag-carmona product_tag-jacobo product_tag-room product_tag-step-to-my product_tag-tabla product_tag-the-room has-post-thumbnail taxable shipping-taxable purchasable product-type-variable tcol-md-3 tcol-sm-4 tcol-xs-6 tcol-ss-12 skateshop tablas kad_product"})
     for elemento in list_elementos:
-        url_imagen = elemento.find("div", {"class": "https://i2.wp.com/www.theroomsk8.com/wp-content/uploads/2021/11/step-to-my-room-2.jpg?fit=2000%2C2000&ssl=1&resize=300%2C300"})
+        url_imagen = elemento.find("div", {"class": "kad_img_flip image_flip_front"}).find('img').attrs['src']
+        nombre = elemento.find("div", {"class": "details_product_item"}).find("div", {"class": "product_details"}).find("a",{"class": "product_item_link product_title_link"}).find('h5').text
+        precio = elemento.find("")
         
 
 
